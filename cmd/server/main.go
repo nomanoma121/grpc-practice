1165259980ccc1797c40d164ba425fe39e34ddb4
nomanoma121 <takeuchi.k.747@ms.saitama-u.ac.jp>
2025-08-26 00:38:32 +0900

    feat(server): リアルタイム監視を追加

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -11,20 +11,21 @@ import (
 
 	todopb "todo-grpc/gen/todo"
 
+	"github.com/google/uuid"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 type TodoStore struct {
-	mu    sync.RWMutex
-	todos map[string]*todopb.Todo
+	mu        sync.RWMutex
+	todos     map[string]*todopb.Todo
 	todoIndex int
 }
 
 func NewTodoStore() *TodoStore {
 	return &TodoStore{
-		todos: make(map[string]*todopb.Todo),
+		todos:     make(map[string]*todopb.Todo),
 		todoIndex: 0,
 	}
 }
@@ -63,9 +64,50 @@ func (ts *TodoStore) Delete(id string) {
 	delete(ts.todos, id)
 }
 
+type Broker struct {
+	mu          sync.RWMutex
+	subscribers map[string]chan *todopb.Todo
+}
+
+func NewBroker() *Broker {
+	return &Broker{
+		subscribers: make(map[string]chan *todopb.Todo),
+	}
+}
+
+func (b *Broker) Subscribe() (string, chan *todopb.Todo) {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	id := uuid.New().String()
+	ch := make(chan *todopb.Todo)
+	b.subscribers[id] = ch
+	return id, ch
+}
+
+func (b *Broker) Unsubscribe(id string) {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	if ch, ok := b.subscribers[id]; ok {
+		delete(b.subscribers, id)
+		close(ch)
+	}
+}
+
+func (b *Broker) Publish(todo *todopb.Todo) {
+	b.mu.RLock()
+	defer b.mu.RUnlock()
+
+	for _, ch := range b.subscribers {
+		ch <- todo
+	}
+}
+
 type todoServiceServer struct {
 	todopb.UnimplementedTodoServiceServer
-	store *TodoStore
+	store  *TodoStore
+	broker *Broker
 }
 
 func (s *todoServiceServer) CreateTodo(ctx context.Context, req *todopb.CreateTodoRequest) (*todopb.Todo, error) {
@@ -76,6 +118,7 @@ func (s *todoServiceServer) CreateTodo(ctx context.Context, req *todopb.CreateTo
 	}
 	s.store.Add(todo)
 	s.store.todoIndex++
+	s.broker.Publish(todo)
 	return todo, nil
 }
 
@@ -102,12 +145,37 @@ func (s *todoServiceServer) UpdateTodo(ctx context.Context, req *todopb.UpdateTo
 	}
 
 	s.store.Update(todo)
+	s.broker.Publish(todo)
 	return todo, nil
 }
 
 func (s *todoServiceServer) DeleteTodo(ctx context.Context, req *todopb.DeleteTodoRequest) (*emptypb.Empty, error) {
 	s.store.Delete(req.Id)
+	s.broker.Publish(&todopb.Todo{Id: req.Id})
 	return &emptypb.Empty{}, nil
+}
+
+func (s *todoServiceServer) WatchTodos(req *emptypb.Empty, stream todopb.TodoService_WatchTodosServer) error {
+	log.Println("WatchTodos stream started for a new client")
+
+	id, ch := s.broker.Subscribe()
+	defer func() {
+		s.broker.Unsubscribe(id)
+	}()
+
+	ctx := stream.Context()
+
+	for {
+		select {
+		case todo := <-ch:
+			if err := stream.Send(todo); err != nil {
+				return err
+			}
+
+		case <-ctx.Done():
+			return nil
+		}
+	}
 }
 
 func main() {
@@ -122,9 +190,11 @@ func main() {
 	s := grpc.NewServer()
 
 	store := NewTodoStore()
+	broker := NewBroker()
 
 	server := &todoServiceServer{
-		store: store,
+		store:  store,
+		broker: broker,
 	}
 
 	todopb.RegisterTodoServiceServer(s, server)
